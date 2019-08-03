7819869c522d596c61aeeedf974029ec16e7b25e
jboe26 <joshrboepple@outlook.com>
2019-08-03 19:26:47 -0400

    character var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,35 @@
-var characters
-var enemies
-var defenders
+$(document).ready(function() {
 
+    // Make our variables global to the runtime of our application
+    var characters = 0;
+    var enemies = 0;
+    var defenders = 0;
+    var attack = 0;
+    
+
+    // Use a function to initialize our calculator.
+    // This way when the user hits clear, we can guarantee a reset of the app.
+    function chooseCharacter() {
+        characters = 0;
+        enemies = 0;
+        defenders = 0;
+        attack = 0;
+        
+
+      $("#first-number, #second-number, #operator, #result").empty();
+    }
+
+    $(".characters").on("click", function() {
+
+
+      if (chooseCharacter) {
+    
+    
+    }
+    
+    $(".yours").on("click", function() {
+
+     
+    $(".enemies").on("click", function() {
+
+    }
