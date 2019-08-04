b5b3c80de98f6a85cee60504817b20802b28d9f2
jboe26 <joshrboepple@outlook.com>
2019-08-04 16:01:24 -0400

    changed background color

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,31 +5,28 @@ $(document).ready(function() {
     var enemies = 0;
     var defenders = 0;
     var attack = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-
-    // Use a function to initialize our calculator.
-    // This way when the user hits clear, we can guarantee a reset of the app.
-    function chooseCharacter() {
-        characters = 0;
-        enemies = 0;
-        defenders = 0;
-        attack = 0;
-        
-
-      $("#first-number, #second-number, #operator, #result").empty();
-    }
-
-    $(".characters").on("click", function() {
-
-
-      if (chooseCharacter) {
-    
-    
-    }
-    
-    $(".yours").on("click", function() {
-
-     
-    $(".enemies").on("click", function() {
-
-    }
+}
